1b9f864b4f462e62866109c43faf554b3a7e69c7
joonsipo <joonas.siponen@metropolia.fi>
2026-01-19 16:37:49 +0200

    Assingment 6

---
+++ Moduuli 2/Assignment 6.py
@@ -0,0 +1,10 @@
+import random
+
+numero1 = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+numero2 = str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6))
+str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+
+str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6))
+print(f"3-digit code: {numero1}")
+print(f"4-digit code: {numero2}")
