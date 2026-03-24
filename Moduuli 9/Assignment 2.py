05532770dba51b29fbd9c47d81ec58b25a22fb33
joonsipo <joonas.siponen@metropolia.fi>
2026-03-24 16:26:42 +0200

    moduuli 9

---
+++ Moduuli 9/Assignment 2.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self, license_plate, maximum_speed):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    def accelerate(self, speed_change):
+        new_speed = self.current_speed + speed_change
+
+        if new_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        elif new_speed < 0:
+            self.current_speed = 0
+        else:
+            self.current_speed = new_speed
